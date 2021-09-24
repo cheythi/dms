ebb1b9e70d5460899059e8563cd8aa545cae30e4
cheythi <chethidasanayaka@gmail.com>
2021-09-24 13:19:18 +0530

    Finalized sidebar design

--- application/views/home_loan/main.php
+++ application/views/home_loan/main.php
@@ -4,12 +4,12 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Advanced Form</h1>
+            <h1>Home Loans</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Advanced Form</li>
+              <li class="breadcrumb-item active">Home Loans</li>
             </ol>
           </div>
         </div>
