af64ececcb234ae1da3310c41495fb689bec9ee6
cheythi <chethidasanayaka@gmail.com>
2021-10-02 14:15:21 +0530

    savings accounts UI

--- application/views/savings_acc/main.php
+++ application/views/savings_acc/main.php
@@ -29,68 +29,64 @@
             
               <div class="col-lg-3 col-6">
                 <!-- small card -->
-                <div class="small-box bg-info">
+                <div class="small-box bg-gradient-success">
                   <div class="inner">
-                    <h3>150</h3>
+                    <h3>1898</h3>
 
-                    <p>New Orders</p>
+                    <p>New Total Accounts</p>
                   </div>
                   <div class="icon">
-                    <i class="fas fa-shopping-cart"></i>
                   </div>
                   <a href="#" class="small-box-footer">
-                    More info <i class="fas fa-arrow-circle-right"></i>
+                    View Details <i class="fas fa-arrow-circle-right"></i>
                   </a>
                 </div>
               </div>
 
               <div class="col-lg-3 col-6">
                 <!-- small card -->
-                <div class="small-box bg-info">
+                <div class="small-box bg-gradient-info">
                   <div class="inner">
-                    <h3>150</h3>
+                    <h3>380</h3>
 
-                    <p>New Orders</p>
+                    <p>New Children Accounts</p>
                   </div>
                   <div class="icon">
-                    <i class="fas fa-shopping-cart"></i>
                   </div>
                   <a href="#" class="small-box-footer">
-                    More info <i class="fas fa-arrow-circle-right"></i>
+                    View details <i class="fas fa-arrow-circle-right"></i>
                   </a>
                 </div>
               </div>
 
               <div class="col-lg-3 col-6">
                 <!-- small card -->
-                <div class="small-box bg-info">
+                <div class="small-box bg-gradient-warning">
                   <div class="inner">
-                    <h3>150</h3>
+                    <h3>965</h3>
 
-                    <p>New Orders</p>
+                    <p>New Teen plus Accounts</p>
                   </div>
                   <div class="icon">
-                    <i class="fas fa-shopping-cart"></i>
                   </div>
                   <a href="#" class="small-box-footer">
-                    More info <i class="fas fa-arrow-circle-right"></i>
+                    View details <i class="fas fa-arrow-circle-right"></i>
                   </a>
                 </div>
               </div>
 
               <div class="col-lg-3 col-6">
                 <!-- small card -->
-                <div class="small-box bg-info">
+                <div class="small-box bg-gradient-danger">
                   <div class="inner">
-                    <h3>150</h3>
+                    <h3>553</h3>
 
-                    <p>New Orders</p>
+                    <p>New Senior Citizens Accounts</p>
                   </div>
                   <div class="icon">
-                    <i class="fas fa-shopping-cart"></i>
                   </div>
                   <a href="#" class="small-box-footer">
-                    More info <i class="fas fa-arrow-circle-right"></i>
+                    View details <i class="fas fa-arrow-circle-right"></i>
                   </a>
                 </div>
               </div>
@@ -100,12 +96,44 @@
             <div class="row">
             
               <div class="col-md-3">
-                <div class="card card-danger">
+                <div class="card card-success">
                   <div class="card-header">
-                    <h3 class="card-title">Danger</h3>
+                    <h3 class="card-title">Account Informations</h3>
                   </div>
                   <div class="card-body">
-                    The body of the card
+                    <li>Account owner details</li>
+                    <li>Account Expenses details</li>
+                  </div>
+                  <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+              </div>
+
+              <div class="col-md-3">
+                <div class="card card-info">
+                  <div class="card-header">
+                    <h3 class="card-title">Children Account Details</h3>
+                  </div>
+                  <div class="card-body">
+                    <li>Birthday details</li>
+                    <li>Prizes</li>
+                    <li>Children day benefits</li>
+                  </div>
+                  <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+              </div>
+
+              <div class="col-md-3">
+                <div class="card card-warning">
+                  <div class="card-header">
+                    <h3 class="card-title">Teen Account Details</h3>
+                  </div>
+                  <div class="card-body">
+                    <li>Online banking applications</li>
+                    <li>Online passbook activation</li>
+                    <li>SMS Alert</li>
+                    <li>Exchange rates</li>
                   </div>
                   <!-- /.card-body -->
                 </div>
@@ -115,36 +143,13 @@
               <div class="col-md-3">
                 <div class="card card-danger">
                   <div class="card-header">
-                    <h3 class="card-title">Danger</h3>
+                    <h3 class="card-title">Senior Citizen Account Details</h3>
                   </div>
                   <div class="card-body">
-                    The body of the card
-                  </div>
-                  <!-- /.card-body -->
-                </div>
-                <!-- /.card -->
-              </div>
-
-              <div class="col-md-3">
-                <div class="card card-danger">
-                  <div class="card-header">
-                    <h3 class="card-title">Danger</h3>
-                  </div>
-                  <div class="card-body">
-                    The body of the card
-                  </div>
-                  <!-- /.card-body -->
-                </div>
-                <!-- /.card -->
-              </div>
-
-              <div class="col-md-3">
-                <div class="card card-danger">
-                  <div class="card-header">
-                    <h3 class="card-title">Danger</h3>
-                  </div>
-                  <div class="card-body">
-                    The body of the card
+                    <li>Interest rates</li>
+                    <li>Retirement plan details</li>
+                    <li>Loan details</li>
+                    <li>Insurance details</li>
                   </div>
                   <!-- /.card-body -->
                 </div>
